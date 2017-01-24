060b3f1bc26e6f46d2c19f9eede564e6a222f17c
@slovacus <slovacus@gmail.com>
2017-01-24 09:38:29 -0500

    !#1 Fix: clean test

--- script/test.sh
+++ script/test.sh
@@ -3,6 +3,3 @@
 
 # shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-ansible-playbook tests/test.yaml -i tests/inventory --syntax-check
-ansible-playbook -i tests/inventory tests/test.yaml --connection=local
